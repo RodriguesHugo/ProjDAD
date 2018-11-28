9f6eb75f5cebac8580832389afb24d060bd664f4
Hugo Rodrigues <macakixo@gmail.com>
2018-11-28 14:34:16 +0000

    itemList div

--- resources/js/components/itemList.vue
+++ resources/js/components/itemList.vue
@@ -1,24 +1,26 @@
 <template>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Type</th>
-                <th>Description</th>
-                <th>Price</th>
-                <th>Photo</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(item,index) in items" :key="item.id">
-                <td>{{item.name}}</td>
-                <td>{{item.type}}</td>
-                <td>{{item.description}}</td>
-                <td>{{item.price}}</td>
-                <td>{{item.photo_url}}</td>
-            </tr>
-        </tbody>
-    </table>
+    <div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Photo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item,index) in items" :key="item.id">
+                    <td>{{item.name}}</td>
+                    <td>{{item.type}}</td>
+                    <td>{{item.description}}</td>
+                    <td>{{item.price}}</td>
+                    <td>{{item.photo_url}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 <script>
 module.exports = {
